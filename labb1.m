0d5ae185c219a6bdf733caae5c9a77fa788182f4
Yunshan <yunshan0611@gmail.com>
2026-09-25 15:59:03 +0200

    klar med fixpunkt iter

--- labb1.m
+++ labb1.m
@@ -5,19 +5,22 @@ R_m = 0.7;
 alpha = 5.7;
 Z = 5;
 
-fixpoint_iter = @(P) P + 2.*r.*P.*(1 - (P/K)) - 2.*R_m.*Z.*(P.^2)./(alpha.^2 + P.^2);  
+f = @(P) r.*P.*(1 - P./K) - R_m.*Z.*(P.^2)/(alpha.^2 + P.^2); 
 
 % Plotting fixpoint_iter
-x = linspace(-10, 300, 100000);
-plot(x, fixpoint_iter(x));
+x = linspace(0, 100, 100000);
+plot(x, f(x));
 
-% Startgissning = 130 avläses från grafen 
-start_guess = 130;
+% startgissning = 13 (1)
+start_guess = 90;
+% startgissning = 95 (2)
+% start_guess = 95;
 
-% iteration 
+fixpoint_iter = @(P) P + 2.*r.*P.*(1 - (P/K)) - 2.*R_m.*Z.*(P.^2)./(alpha.^2 + P.^2); 
 tol = 1e-10;
 diff = 1;
 curr_iter = fixpoint_iter(start_guess);
+fprintf("startgissning: %d\n", start_guess)
 
 while diff > tol
     next_iter = fixpoint_iter(curr_iter);
